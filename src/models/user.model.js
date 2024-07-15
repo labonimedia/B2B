345ff6596077db763b312c44af6971c67fdbb8ae
bhusnarsd <bhusnarsd@gmail.com>
2024-07-15 18:03:51 +0530

    chnages

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -38,7 +38,7 @@ const userSchema = mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['superadmin', 'manufacture'],
+      enum: ['superadmin', 'manufacture', 'wholesaler'],
       default: 'user',
     },
     code: {
