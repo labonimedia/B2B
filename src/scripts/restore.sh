090b4cccdb04068783e4da13a328dc496fd1e708
bhusnarsd <bhusnarsd@gmail.com>
2024-07-18 11:43:03 +0530

    chnages

--- src/scripts/restore.sh
+++ src/scripts/restore.sh
@@ -14,7 +14,7 @@
 # Restore the database
 # mongorestore --host localhost --port 27017 --db DMF ../dbs/DMF
 
-mongorestore --host localhost --port 27017 --nsInclude 'DMF.*' ../dbs
+mongorestore --host localhost --port 27017 --nsInclude 'B2B.*' ../dbs
 
 # mongorestore --username root --password VRuAd2Nvmp4ELHh5 --authenticationDatabase admin --archive=$BACKUP_FILE --gzip --drop
 
