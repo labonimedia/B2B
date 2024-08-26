b47ff9d306f141d23b2f1e56c6c8d8e839ea95a4
onkar ungratwar <ungratwaronkar@gmail.com>
2024-08-26 17:58:11 +0530

    emai2CHange

--- src/models/wholesaler.model.js
+++ src/models/wholesaler.model.js
@@ -43,14 +43,14 @@ const wholesalerSchema = mongoose.Schema(
     },
     email2: {
       type: String,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error('Invalid email');
-        }
-      },
+      // unique: true,
+      // trim: true,
+      // lowercase: true,
+      // validate(value) {
+      //   if (!validator.isEmail(value)) {
+      //     throw new Error('Invalid email');
+      //   }
+      // },
     },
     GSTIN: {
       type: String,
