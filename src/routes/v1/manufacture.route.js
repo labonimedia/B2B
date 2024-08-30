c7cc8a13edd30dcdb2a79890a42b6883e0a6d8f7
bhusnarsd <bhusnarsd@gmail.com>
2024-08-30 16:44:24 +0530

    chnages

--- src/routes/v1/manufacture.route.js
+++ src/routes/v1/manufacture.route.js
@@ -6,7 +6,7 @@ const { commonUploadMiddleware } = require('../../utils/upload');
 const router = express.Router();
 
 router.route('/upload/doc/:id').post(
-  auth('superadmin', 'manufacture'),
+  // auth('superadmin', 'manufacture'),
   commonUploadMiddleware([{ name: 'file', maxCount: 1 },{ name: 'profileImg', maxCount: 1 }]),
   manufactureController.fileupload
 );
