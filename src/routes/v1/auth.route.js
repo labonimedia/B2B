0c3379e724b64a57d0823efbf3bbded472a005d9
bhusnarsd <bhsnarsd@gmail.com>
2024-11-28 11:10:02 +0530

    changes in authentication

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -5,12 +5,12 @@ const authController = require('../../controllers/auth.controller');
 
 const router = express.Router();
 
-// router.post('/register', validate(authValidation.register), authController.register);
+router.post('/register', validate(authValidation.register), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
-// router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
-// router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
+router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
+router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
 router.post('/send-verification-email', authController.sendVerificationEmail);
 router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail);
 
