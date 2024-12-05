6383ea8edda1e245d3667866dbde9d87c2dacd31
bhusnarsd <bhsnarsd@gmail.com>
2024-12-05 11:32:35 +0530

    chnages pocounter collection type2

--- src/models/type2.models/po.counter.model.js
+++ src/models/type2.models/po.counter.model.js
@@ -22,7 +22,7 @@ const orderCounterSchema = new mongoose.Schema(
 );
 
 // Create a unique compound index on wholesalerEmail and year
-orderCounterSchema.index({ wholesalerEmail: 1, year: 1 }, { unique: true });
+orderCounterSchema.index({ email: 1, year: 1 }, { unique: true });
 
 const POCounterType2 = mongoose.model('POCounterType2', orderCounterSchema);
 
