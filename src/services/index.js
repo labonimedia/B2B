501bebe08bebfd03ccdf89bb0833f27f6d9c132c
bhusnarsd <bhsnarsd@gmail.com>
2025-01-21 17:34:06 +0530

    chnaes

--- src/services/index.js
+++ src/services/index.js
@@ -99,4 +99,4 @@ module.exports.newCountryCodeService = require('./new.country.service');
 module.exports.stateService = require('./state.service');
 module.exports.cityService = require('./city.service');
 module.exports.cdnPathService = require('./cdn.path.service');
-module.exports.testService = require('./test.service');
+module.exports.cdnChangeService = require('./cdn.change.service');
