72ef832c16d7f9fd70ebc90456047df9ce3931b7
Goodwill407 <142879911+Goodwill407@users.noreply.github.com>
2025-02-12 16:19:15 +0530

    chnages

--- src/utils/queue.js
+++ src/utils/queue.js
@@ -7,7 +7,7 @@ const redisClient = new Redis({
     port: process.env.REDIS_PORT || 6379,
 });
 
-// const notificationQueue = new Bull("notify-wholesaler", { redis: redisClient });
+const notificationQueue = new Bull("notify-wholesaler", { redis: redisClient });
 const autoForwardQueue = new Bull("auto-forward-orders", { redis: redisClient });
 const autoCancelQueue = new Bull("auto-cancel-orders", { redis: redisClient });
 
