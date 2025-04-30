b4f1760a3e76c067925b64b2b6b7644932b05958
onkar ungratwar <ungratwaronkar@gmail.com>
2025-04-30 16:43:45 +0530

    modelchange

--- src/models/type2.models/retailer.cart.model.js
+++ src/models/type2.models/retailer.cart.model.js
@@ -17,7 +17,7 @@ const CartSchema = new mongoose.Schema(
     },
     set: [
       {
-        _id: false,
+        // _id: false,
         // productId: {
         //   type: mongoose.Schema.Types.ObjectId,
         //   ref: 'ProductType2',
