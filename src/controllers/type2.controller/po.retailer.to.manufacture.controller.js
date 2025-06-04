82a5ded0b632124510ed9288fb29039c2b9e0be2
onkar ungratwar <ungratwaronkar@gmail.com>
2025-06-04 10:29:24 +0530

    pickerror

--- src/controllers/type2.controller/po.retailer.to.manufacture.controller.js
+++ src/controllers/type2.controller/po.retailer.to.manufacture.controller.js
@@ -1,5 +1,6 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../../utils/catchAsync');
+const pick = require('../../utils/pick');
 const { poRetailerToManufactureService } = require('../../services');
 
 const createRetailerPurchaseOrderType2 = catchAsync(async (req, res) => {
