a41250272ab0f4a40403900d9890651ed56be9e2
Ungratwar <ungratwaronkar@gmail.com>
2026-02-11 15:56:20 +0530

    code

--- src/services/type2.services/wholesaler.inventory.logs.service.js
+++ src/services/type2.services/wholesaler.inventory.logs.service.js
@@ -46,14 +46,108 @@ const createInventory = async (data) => {
   return WholesalerInventoryLogs.create(data);
 };
 
-const queryInventories = async (filter, options, search) => {
+// const queryInventories = async (filter, options, search) => {
+//   const matchStage = { ...filter };
+
+//   if (search) {
+//     matchStage.$or = [{ designNumber: { $regex: search, $options: 'i' } }, { brandName: { $regex: search, $options: 'i' } }];
+//   }
+
+//   return WholesalerInventoryLogs.paginate(matchStage, options);
+// };
+const queryInventories = async (filter = {}, options = {}, search = '') => {
+  const page = parseInt(options.page) || 1;
+  const limit = parseInt(options.limit) || 10;
+  const skip = (page - 1) * limit;
+
   const matchStage = { ...filter };
 
   if (search) {
-    matchStage.$or = [{ designNumber: { $regex: search, $options: 'i' } }, { brandName: { $regex: search, $options: 'i' } }];
+    matchStage.$or = [
+      { designNumber: { $regex: search, $options: 'i' } },
+      { brandName: { $regex: search, $options: 'i' } },
+    ];
   }
 
-  return WholesalerInventoryLogs.paginate(matchStage, options);
+  const aggregation = await WholesalerInventoryLogs.aggregate([
+    { $match: matchStage },
+
+    { $unwind: '$recordsArray' },
+
+    {
+      $group: {
+        _id: {
+          designNumber: '$designNumber',
+          brandName: '$brandName',
+        },
+
+        totalUpdates: { $sum: 1 },
+
+        totalQuantityAdded: {
+          $sum: {
+            $cond: [
+              { $eq: ['$recordsArray.status', 'stock_added'] },
+              '$recordsArray.updatedQuantity',
+              0,
+            ],
+          },
+        },
+
+        totalQuantityRemoved: {
+          $sum: {
+            $cond: [
+              { $eq: ['$recordsArray.status', 'stock_removed'] },
+              '$recordsArray.updatedQuantity',
+              0,
+            ],
+          },
+        },
+
+        latestUpdatedAt: { $max: '$recordsArray.lastUpdatedAt' },
+
+        entries: {
+          $push: {
+            _id: '$_id',
+            userEmail: '$userEmail',
+            colour: '$colour',
+            colourName: '$colourName',
+            brandSize: '$brandSize',
+            standardSize: '$standardSize',
+            records: '$recordsArray',
+          },
+        },
+      },
+    },
+
+    {
+      $addFields: {
+        designNumber: '$_id.designNumber',
+        brandName: '$_id.brandName',
+      },
+    },
+
+    { $project: { _id: 0 } },
+
+    { $sort: { latestUpdatedAt: -1 } },
+
+    {
+      $facet: {
+        paginatedResults: [{ $skip: skip }, { $limit: limit }],
+        totalCount: [{ $count: 'count' }],
+      },
+    },
+  ]);
+
+  const results = aggregation[0]?.paginatedResults || [];
+  const totalCount = aggregation[0]?.totalCount?.[0]?.count || 0;
+
+  return {
+    results,
+    page,
+    limit,
+    totalPages: Math.ceil(totalCount / limit),
+    totalResults: totalCount,
+  };
 };
 
 const getInventoryById = async (id) => {
