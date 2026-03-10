61838d00e6034e40b7a3b2a8710dcb60f7c0e848
Ungratwar <ungratwaronkar@gmail.com>
2026-03-10 17:08:08 +0530

    changesALlAPi

--- src/controllers/type2.controller/wholesaler.cart.controller.js
+++ src/controllers/type2.controller/wholesaler.cart.controller.js
@@ -1,72 +1,162 @@
-const httpStatus = require('http-status');
-const catchAsync = require('../../utils/catchAsync');
-const ApiError = require('../../utils/ApiError');
-const { wholesalerCartToManufacturerService } = require('../../services');
+// const httpStatus = require('http-status');
+// const catchAsync = require('../../utils/catchAsync');
+// const ApiError = require('../../utils/ApiError');
+// const { wholesalerCartToManufacturerService } = require('../../services');
 
-const addToCart = catchAsync(async (req, res) => {
-  const result = await wholesalerCartToManufacturerService.createWholesalerCart(req.body);
-  res.status(httpStatus.CREATED).send(result);
-});
+// const addToCart = catchAsync(async (req, res) => {
+//   const result = await wholesalerCartToManufacturerService.createWholesalerCart(req.body);
+//   res.status(httpStatus.CREATED).send(result);
+// });
 
-// const getCartByWholesaler = catchAsync(async (req, res) => {
-//   const { wholesalerEmail } = req.query;
+// // const getCartByWholesaler = catchAsync(async (req, res) => {
+// //   const { wholesalerEmail } = req.query;
 
-//   const result = await wholesalerCartToManufacturerService.getCartByWholesaler(wholesalerEmail);
+// //   const result = await wholesalerCartToManufacturerService.getCartByWholesaler(wholesalerEmail);
+
+// //   res.status(httpStatus.OK).send(result);
+// // });
+
+// const getCartByWholesaler = async (req, res) => {
+
+//   const filter = {};
+
+//   if (req.query.wholesalerEmail) {
+//     filter.wholesalerEmail = req.query.wholesalerEmail;
+//   }
+
+//   const options = {
+//     page: parseInt(req.query.page) || 1,
+//     limit: parseInt(req.query.limit) || 10,
+//   };
+
+//   const result = await wholesalerCartToManufacturerService.queryWholesalerCart(filter, options);
+
+//   res.send(result);
+// };
+
+// const getSingleCart = catchAsync(async (req, res) => {
+//   const result = await wholesalerCartToManufacturerService.getCartById(req.params.id);
+
+//   if (!result) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
+//   }
 
 //   res.status(httpStatus.OK).send(result);
 // });
 
-const getCartByWholesaler = async (req, res) => {
+// const updateCart = catchAsync(async (req, res) => {
+//   const result = await wholesalerCartToManufacturerService.updateCart(req.params.id, req.body);
 
-  const filter = {};
+//   res.status(httpStatus.OK).send(result);
+// });
 
-  if (req.query.wholesalerEmail) {
-    filter.wholesalerEmail = req.query.wholesalerEmail;
-  }
+// const deleteCart = catchAsync(async (req, res) => {
+//   await wholesalerCartToManufacturerService.deleteCart(req.params.id);
 
-  const options = {
-    page: parseInt(req.query.page) || 1,
-    limit: parseInt(req.query.limit) || 10,
-  };
+//   res.status(httpStatus.NO_CONTENT).send();
+// });
 
-  const result = await wholesalerCartToManufacturerService.queryWholesalerCart(filter, options);
+// const updateCartSetItem = catchAsync(async (req, res) => {
+//   const result = await wholesalerCartToManufacturerService.updateCartSetItem(req.params.cartId, req.body);
 
-  res.send(result);
-};
+//   res.status(httpStatus.OK).send(result);
+// });
 
-const getSingleCart = catchAsync(async (req, res) => {
+// module.exports = {
+//   addToCart,
+//   getCartByWholesaler,
+//   getSingleCart,
+//   updateCart,
+//   deleteCart,
+//   updateCartSetItem,
+// };
+
+
+const httpStatus = require('http-status');
+const pick = require('../../utils/pick');
+const catchAsync = require('../../utils/catchAsync');
+const ApiError = require('../../utils/ApiError');
+const { wholesalerCartToManufacturerService } = require('../../services');
+
+const createCart = catchAsync(async (req, res) => {
+  const result = await wholesalerCartToManufacturerService.createCart(req.body);
+  res.status(httpStatus.CREATED).send(result);
+});
+
+const queryCart = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['manufacturerEmail', 'wholesalerEmail']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+
+  const result = await wholesalerCartToManufacturerService.queryCart(filter, options);
+
+  res.status(httpStatus.OK).send(result);
+});
+
+const getCartById = catchAsync(async (req, res) => {
   const result = await wholesalerCartToManufacturerService.getCartById(req.params.id);
 
   if (!result) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
   }
 
-  res.status(httpStatus.OK).send(result);
+  res.send(result);
 });
 
-const updateCart = catchAsync(async (req, res) => {
-  const result = await wholesalerCartToManufacturerService.updateCart(req.params.id, req.body);
+const getCartByEmail = catchAsync(async (req, res) => {
+  const { wholesalerEmail } = req.query;
 
-  res.status(httpStatus.OK).send(result);
+  const result = await wholesalerCartToManufacturerService.getCartByEmail(wholesalerEmail);
+
+  res.send(result);
 });
 
-const deleteCart = catchAsync(async (req, res) => {
-  await wholesalerCartToManufacturerService.deleteCart(req.params.id);
+const getCartByEmailToPlaceOrder = catchAsync(async (req, res) => {
+  const { wholesalerEmail, manufacturerEmail } = req.query;
+
+  const result = await wholesalerCartToManufacturerService.getCartByEmailToPlaceOrder(
+    wholesalerEmail,
+    manufacturerEmail
+  );
+
+  res.send(result);
+});
+
+const updateCartById = catchAsync(async (req, res) => {
+  const result = await wholesalerCartToManufacturerService.updateCartById(req.params.id, req.body);
+
+  res.send(result);
+});
+
+const deleteCartById = catchAsync(async (req, res) => {
+  await wholesalerCartToManufacturerService.deleteCartById(req.params.id);
 
   res.status(httpStatus.NO_CONTENT).send();
 });
 
-const updateCartSetItem = catchAsync(async (req, res) => {
-  const result = await wholesalerCartToManufacturerService.updateCartSetItem(req.params.cartId, req.body);
+const updateSetItem = catchAsync(async (req, res) => {
+  const { cartId, setId } = req.params;
 
-  res.status(httpStatus.OK).send(result);
+  const result = await wholesalerCartToManufacturerService.updateSetItem(cartId, setId, req.body);
+
+  res.send(result);
+});
+
+const deleteCartSetItem = catchAsync(async (req, res) => {
+  const { cartId, setId } = req.params;
+
+  await wholesalerCartToManufacturerService.deleteCartSetItem(cartId, setId);
+
+  res.status(httpStatus.NO_CONTENT).send();
 });
 
 module.exports = {
-  addToCart,
-  getCartByWholesaler,
-  getSingleCart,
-  updateCart,
-  deleteCart,
-  updateCartSetItem,
+  createCart,
+  queryCart,
+  getCartByEmail,
+  getCartByEmailToPlaceOrder,
+  getCartById,
+  updateCartById,
+  deleteCartById,
+  updateSetItem,
+  deleteCartSetItem,
 };
--- src/services/type2.services/wholesaler.cart.service.js
+++ src/services/type2.services/wholesaler.cart.service.js
@@ -1,62 +1,114 @@
-const httpStatus = require('http-status');
-const { WholesalerCartToManufacturer, Wholesaler, Manufacture, POWholesalerToManufacturer } = require('../../models');
-const ApiError = require('../../utils/ApiError');
+// const httpStatus = require('http-status');
+// const { WholesalerCartToManufacturer, Wholesaler, Manufacture, POWholesalerToManufacturer } = require('../../models');
+// const ApiError = require('../../utils/ApiError');
 
-/**
- * Create or update cart
- */
-const createWholesalerCart = async (reqBody) => {
-  const { wholesalerEmail, manufacturerEmail, set } = reqBody;
+// /**
+//  * Create or update cart
+//  */
+// const createWholesalerCart = async (reqBody) => {
+//   const { wholesalerEmail, manufacturerEmail, set } = reqBody;
 
-  const existingCart = await WholesalerCartToManufacturer.findOne({
-    wholesalerEmail,
-    manufacturerEmail,
-  });
+//   const existingCart = await WholesalerCartToManufacturer.findOne({
+//     wholesalerEmail,
+//     manufacturerEmail,
+//   });
 
-  if (existingCart) {
-    set.forEach((newItem) => {
-      const existingItem = existingCart.set.find(
-        (item) => item.designNumber === newItem.designNumber && item.colour === newItem.colour && item.size === newItem.size
-      );
+//   if (existingCart) {
+//     set.forEach((newItem) => {
+//       const existingItem = existingCart.set.find(
+//         (item) => item.designNumber === newItem.designNumber && item.colour === newItem.colour && item.size === newItem.size
+//       );
 
-      if (existingItem) {
-        existingItem.quantity += newItem.quantity;
-      } else {
-        existingCart.set.push(newItem);
-      }
-    });
+//       if (existingItem) {
+//         existingItem.quantity += newItem.quantity;
+//       } else {
+//         existingCart.set.push(newItem);
+//       }
+//     });
 
-    await existingCart.save();
-    return existingCart;
-  } else {
-    const newCart = await WholesalerCartToManufacturer.create(reqBody);
-    return newCart;
-  }
-};
+//     await existingCart.save();
+//     return existingCart;
+//   } else {
+//     const newCart = await WholesalerCartToManufacturer.create(reqBody);
+//     return newCart;
+//   }
+// };
 
-/**
- * Query cart
- */
+// /**
+//  * Query cart
+//  */
+
+// // const queryWholesalerCart = async (filter, options) => {
+// //   const cartItems = await WholesalerCartToManufacturer.paginate(filter, options);
+
+// //   if (cartItems.results.length === 0) {
+// //     return cartItems;
+// //   }
+
+// //   const manufacturerEmails = [...new Set(cartItems.results.map((item) => item.manufacturerEmail))];
+
+// //   const manufacturers = await Manufacture.find({
+// //     email: { $in: manufacturerEmails },
+// //   }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN');
+
+// //   const wholesaler = await Wholesaler.findOne({
+// //     email: filter.wholesalerEmail,
+// //   }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN');
+
+// //   const manufacturerMap = manufacturers.reduce((acc, m) => {
+// //     acc[m.email] = {
+// //       email: m.email,
+// //       fullName: m.fullName,
+// //       companyName: m.companyName,
+// //       address: m.address,
+// //       state: m.state,
+// //       country: m.country,
+// //       pinCode: m.pinCode,
+// //       mobNumber: m.mobNumber,
+// //       profileImg: m.profileImg,
+// //       GSTIN: m.GSTIN,
+// //     };
+// //     return acc;
+// //   }, {});
+
+// //   cartItems.results = cartItems.results.map((item) => ({
+// //     ...item.toObject(),
+// //     manufacturer: manufacturerMap[item.manufacturerEmail] || null,
+// //     wholesaler,
+// //   }));
+
+// //   return cartItems;
+// // };
 
 // const queryWholesalerCart = async (filter, options) => {
 //   const cartItems = await WholesalerCartToManufacturer.paginate(filter, options);
 
-//   if (cartItems.results.length === 0) {
+//   if (!cartItems.results || cartItems.results.length === 0) {
 //     return cartItems;
 //   }
 
+//   // manufacturer emails
 //   const manufacturerEmails = [...new Set(cartItems.results.map((item) => item.manufacturerEmail))];
 
+//   // fetch manufacturers
 //   const manufacturers = await Manufacture.find({
 //     email: { $in: manufacturerEmails },
-//   }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN');
+//   }).lean();
 
-//   const wholesaler = await Wholesaler.findOne({
-//     email: filter.wholesalerEmail,
-//   }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN');
+//   // fetch wholesaler
+//   let wholesaler = null;
 
-//   const manufacturerMap = manufacturers.reduce((acc, m) => {
-//     acc[m.email] = {
+//   if (filter.wholesalerEmail) {
+//     wholesaler = await Wholesaler.findOne({
+//       email: filter.wholesalerEmail,
+//     }).lean();
+//   }
+
+//   // manufacturer map
+//   const manufacturerMap = {};
+
+//   manufacturers.forEach((m) => {
+//     manufacturerMap[m.email] = {
 //       email: m.email,
 //       fullName: m.fullName,
 //       companyName: m.companyName,
@@ -68,135 +120,269 @@ const createWholesalerCart = async (reqBody) => {
 //       profileImg: m.profileImg,
 //       GSTIN: m.GSTIN,
 //     };
-//     return acc;
-//   }, {});
+//   });
 
-//   cartItems.results = cartItems.results.map((item) => ({
-//     ...item.toObject(),
-//     manufacturer: manufacturerMap[item.manufacturerEmail] || null,
-//     wholesaler,
-//   }));
+//   // attach manufacturer + wholesaler
+//   cartItems.results = cartItems.results.map((item) => {
+//     const cart = item.toObject ? item.toObject() : item;
+
+//     return {
+//       ...cart,
+//       manufacturer: manufacturerMap[cart.manufacturerEmail] || null,
+//       wholesaler: wholesaler || null,
+//     };
+//   });
 
 //   return cartItems;
 // };
 
-const queryWholesalerCart = async (filter, options) => {
-  const cartItems = await WholesalerCartToManufacturer.paginate(filter, options);
+// /**
+//  * Get cart by id
+//  */
+// const getCartById = async (id) => {
+//   const cart = await WholesalerCartToManufacturer.findById(id);
 
-  if (!cartItems.results || cartItems.results.length === 0) {
-    return cartItems;
+//   if (!cart) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
+//   }
+
+//   return cart;
+// };
+
+// /**
+//  * Generate PO data from cart
+//  */
+// const generatePOFromCart = async (cartId) => {
+//   const cart = await WholesalerCartToManufacturer.findById(cartId);
+
+//   if (!cart) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
+//   }
+
+//   const wholesaler = await Wholesaler.findOne({
+//     email: cart.wholesalerEmail,
+//   }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN logo');
+
+//   const manufacturer = await Manufacture.findOne({
+//     email: cart.manufacturerEmail,
+//   }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN logo');
+
+//   const lastPO = await POWholesalerToManufacturer.findOne({
+//     wholesalerEmail: cart.wholesalerEmail,
+//   })
+//     .sort({ poNumber: -1 })
+//     .lean();
+
+//   const poNumber = lastPO ? lastPO.poNumber + 1 : 1;
+
+//   return {
+//     ...cart.toObject(),
+//     poNumber,
+//     wholesaler,
+//     manufacturer,
+//   };
+// };
+
+// /**
+//  * Get cart by wholesaler
+//  */
+// const getCartByWholesaler = async (wholesalerEmail) => {
+//   return WholesalerCartToManufacturer.find({
+//     wholesalerEmail,
+//   }).sort({ createdAt: -1 });
+// };
+
+// /**
+//  * Update cart
+//  */
+// const updateCart = async (cartId, updateBody) => {
+//   const cart = await getCartById(cartId);
+
+//   Object.assign(cart, updateBody);
+
+//   await cart.save();
+
+//   return cart;
+// };
+
+// /**
+//  * Delete cart
+//  */
+// const deleteCart = async (cartId) => {
+//   const cart = await getCartById(cartId);
+
+//   await cart.remove();
+
+//   return cart;
+// };
+
+// /**
+//  * Delete set item
+//  */
+// const deleteCartSetItem = async (cartId, setId) => {
+//   const cart = await getCartById(cartId);
+
+//   const initialLength = cart.set.length;
+
+//   cart.set = cart.set.filter((item) => item._id.toString() !== setId);
+
+//   if (cart.set.length === initialLength) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'Set item not found');
+//   }
+
+//   await cart.save();
+
+//   return cart;
+// };
+
+// /**
+//  * Update set item
+//  */
+// const updateSetItem = async (cartId, setId, updateBody) => {
+//   const cart = await getCartById(cartId);
+
+//   const item = cart.set.id(setId);
+
+//   if (!item) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'Set item not found');
+//   }
+
+//   Object.keys(updateBody).forEach((key) => {
+//     item[key] = updateBody[key];
+//   });
+
+//   await cart.save();
+
+//   return cart;
+// };
+
+// module.exports = {
+//   createWholesalerCart,
+//   queryWholesalerCart,
+//   generatePOFromCart,
+//   getCartByWholesaler,
+//   getCartById,
+//   updateCart,
+//   deleteCart,
+//   deleteCartSetItem,
+//   updateSetItem,
+// };
+
+const httpStatus = require('http-status');
+const {
+  WholesalerCartToManufacturer,
+  Wholesaler,
+  Manufacture,
+  User,
+} = require('../../models');
+
+const ApiError = require('../../utils/ApiError');
+
+/**
+ * CREATE CART
+ */
+
+const createCart = async (reqBody) => {
+  const { wholesalerEmail, manufacturerEmail, set } = reqBody;
+
+  const existingCart = await WholesalerCartToManufacturer.findOne({
+    wholesalerEmail,
+    manufacturerEmail,
+  });
+
+  if (existingCart) {
+    set.forEach((newItem) => {
+      const existingItem = existingCart.set.find(
+        (item) =>
+          item.designNumber === newItem.designNumber &&
+          item.colour === newItem.colour &&
+          item.size === newItem.size
+      );
+
+      if (existingItem) {
+        existingItem.quantity += newItem.quantity;
+      } else {
+        existingCart.set.push(newItem);
+      }
+    });
+
+    await existingCart.save();
+
+    return existingCart;
   }
 
-  // manufacturer emails
+  return WholesalerCartToManufacturer.create(reqBody);
+};
+
+/**
+ * QUERY CART
+ */
+
+const queryCart = async (filter, options) => {
+  const cartItems = await WholesalerCartToManufacturer.paginate(filter, options);
+
+  if (!cartItems.results.length) return cartItems;
+
   const manufacturerEmails = [...new Set(cartItems.results.map((item) => item.manufacturerEmail))];
 
-  // fetch manufacturers
   const manufacturers = await Manufacture.find({
     email: { $in: manufacturerEmails },
-  }).lean();
+  }).select('email fullName companyName address state country pinCode mobNumber GSTIN');
 
-  // fetch wholesaler
-  let wholesaler = null;
+  let wholesaler;
 
   if (filter.wholesalerEmail) {
     wholesaler = await Wholesaler.findOne({
       email: filter.wholesalerEmail,
-    }).lean();
+    }).select('email fullName companyName address state country pinCode mobNumber GSTIN');
   }
 
-  // manufacturer map
-  const manufacturerMap = {};
+  const manufacturerMap = manufacturers.reduce((acc, m) => {
+    acc[m.email] = m;
+    return acc;
+  }, {});
 
-  manufacturers.forEach((m) => {
-    manufacturerMap[m.email] = {
-      email: m.email,
-      fullName: m.fullName,
-      companyName: m.companyName,
-      address: m.address,
-      state: m.state,
-      country: m.country,
-      pinCode: m.pinCode,
-      mobNumber: m.mobNumber,
-      profileImg: m.profileImg,
-      GSTIN: m.GSTIN,
-    };
-  });
-
-  // attach manufacturer + wholesaler
-  cartItems.results = cartItems.results.map((item) => {
-    const cart = item.toObject ? item.toObject() : item;
-
-    return {
-      ...cart,
-      manufacturer: manufacturerMap[cart.manufacturerEmail] || null,
-      wholesaler: wholesaler || null,
-    };
-  });
+  cartItems.results = cartItems.results.map((item) => ({
+    ...item.toObject(),
+    manufacturer: manufacturerMap[item.manufacturerEmail] || null,
+    wholesaler,
+  }));
 
   return cartItems;
 };
 
 /**
- * Get cart by id
+ * GET CART BY ID
  */
+
 const getCartById = async (id) => {
-  const cart = await WholesalerCartToManufacturer.findById(id);
+  return WholesalerCartToManufacturer.findById(id);
+};
+
+/**
+ * GET CART BY EMAIL
+ */
+
+const getCartByEmail = async (wholesalerEmail) => {
+  const carts = await WholesalerCartToManufacturer.find({ wholesalerEmail });
+
+  if (!carts.length) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'No carts found');
+  }
+
+  return carts;
+};
+
+/**
+ * UPDATE CART
+ */
+
+const updateCartById = async (id, updateBody) => {
+  const cart = await getCartById(id);
 
   if (!cart) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
   }
-
-  return cart;
-};
-
-/**
- * Generate PO data from cart
- */
-const generatePOFromCart = async (cartId) => {
-  const cart = await WholesalerCartToManufacturer.findById(cartId);
-
-  if (!cart) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
-  }
-
-  const wholesaler = await Wholesaler.findOne({
-    email: cart.wholesalerEmail,
-  }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN logo');
-
-  const manufacturer = await Manufacture.findOne({
-    email: cart.manufacturerEmail,
-  }).select('email fullName companyName address state country pinCode mobNumber profileImg GSTIN logo');
-
-  const lastPO = await POWholesalerToManufacturer.findOne({
-    wholesalerEmail: cart.wholesalerEmail,
-  })
-    .sort({ poNumber: -1 })
-    .lean();
-
-  const poNumber = lastPO ? lastPO.poNumber + 1 : 1;
-
-  return {
-    ...cart.toObject(),
-    poNumber,
-    wholesaler,
-    manufacturer,
-  };
-};
-
-/**
- * Get cart by wholesaler
- */
-const getCartByWholesaler = async (wholesalerEmail) => {
-  return WholesalerCartToManufacturer.find({
-    wholesalerEmail,
-  }).sort({ createdAt: -1 });
-};
-
-/**
- * Update cart
- */
-const updateCart = async (cartId, updateBody) => {
-  const cart = await getCartById(cartId);
 
   Object.assign(cart, updateBody);
 
@@ -206,10 +392,15 @@ const updateCart = async (cartId, updateBody) => {
 };
 
 /**
- * Delete cart
+ * DELETE CART
  */
-const deleteCart = async (cartId) => {
-  const cart = await getCartById(cartId);
+
+const deleteCartById = async (id) => {
+  const cart = await getCartById(id);
+
+  if (!cart) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Cart not found');
+  }
 
   await cart.remove();
 
@@ -217,27 +408,9 @@ const deleteCart = async (cartId) => {
 };
 
 /**
- * Delete set item
+ * UPDATE SET ITEM
  */
-const deleteCartSetItem = async (cartId, setId) => {
-  const cart = await getCartById(cartId);
 
-  const initialLength = cart.set.length;
-
-  cart.set = cart.set.filter((item) => item._id.toString() !== setId);
-
-  if (cart.set.length === initialLength) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Set item not found');
-  }
-
-  await cart.save();
-
-  return cart;
-};
-
-/**
- * Update set item
- */
 const updateSetItem = async (cartId, setId, updateBody) => {
   const cart = await getCartById(cartId);
 
@@ -256,14 +429,27 @@ const updateSetItem = async (cartId, setId, updateBody) => {
   return cart;
 };
 
+/**
+ * DELETE SET ITEM
+ */
+
+const deleteCartSetItem = async (cartId, setId) => {
+  const cart = await getCartById(cartId);
+
+  cart.set = cart.set.filter((item) => item._id.toString() !== setId);
+
+  await cart.save();
+
+  return cart;
+};
+
 module.exports = {
-  createWholesalerCart,
-  queryWholesalerCart,
-  generatePOFromCart,
-  getCartByWholesaler,
+  createCart,
+  queryCart,
+  getCartByEmail,
   getCartById,
-  updateCart,
-  deleteCart,
-  deleteCartSetItem,
+  updateCartById,
+  deleteCartById,
   updateSetItem,
+  deleteCartSetItem,
 };
