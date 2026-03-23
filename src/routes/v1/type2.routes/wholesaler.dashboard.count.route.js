ef28c4183582df3c29beaf0761e42882fe783485
Ungratwar <ungratwaronkar@gmail.com>
2026-03-23 14:19:46 +0530

    wholesalerdashboardcount

--- src/routes/v1/type2.routes/wholesaler.dashboard.count.route.js
+++ src/routes/v1/type2.routes/wholesaler.dashboard.count.route.js
@@ -1,3 +1,16 @@
+// const express = require('express');
+// const auth = require('../../../middlewares/auth');
+// const { wholesalerDashboardController } = require('../../../controllers');
+
+// const router = express.Router();
+
+// router.get(
+//   '/dashboard-counts/:role/:email',
+//   auth('superadmin', 'manufacture', 'wholesaler', 'retailer'),
+//   wholesalerDashboardController.wholesalerDashboardCountsController
+// );
+
+// module.exports = router;
 const express = require('express');
 const auth = require('../../../middlewares/auth');
 const { wholesalerDashboardController } = require('../../../controllers');
