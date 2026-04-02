1ca3b795de634b087faf1278af5b407e13a5e06b
Ungratwar <ungratwaronkar@gmail.com>
2026-04-02 15:31:00 +0530

    return

--- src/controllers/type2.controller/return.retailer.to.manufacture.controller.js
+++ src/controllers/type2.controller/return.retailer.to.manufacture.controller.js
@@ -2,10 +2,10 @@ const httpStatus = require('http-status');
 const pick = require('../../utils/pick');
 const ApiError = require('../../utils/ApiError');
 const catchAsync = require('../../utils/catchAsync');
-const { wToMReturnRequestService } = require('../../services');
+const { rtoMReturnRequestService } = require('../../services');
 
 const returnRequest = catchAsync(async (req, res) => {
-  const data = await wToMReturnRequestService.createW2MReturnRequest(req.body);
+  const data = await rtoMReturnRequestService.createMtoRReturnRequest(req.body);
   res.status(httpStatus.CREATED).send(data);
 });
 
@@ -14,12 +14,12 @@ const queryReturnRequest = catchAsync(async (req, res) => {
 
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
 
-  const result = await wToMReturnRequestService.queryW2MReturnRequest(filter, options);
+  const result = await rtoMReturnRequestService.queryMtoRReturnRequest(filter, options);
   res.send(result);
 });
 
 const getReturnRequestById = catchAsync(async (req, res) => {
-  const result = await wToMReturnRequestService.getW2MReturnRequestById(req.params.id);
+  const result = await rtoMReturnRequestService.getMtoRReturnRequestById(req.params.id);
 
   if (!result) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Return Request not found');
@@ -29,12 +29,12 @@ const getReturnRequestById = catchAsync(async (req, res) => {
 });
 
 const updateReturnRequestById = catchAsync(async (req, res) => {
-  const result = await wToMReturnRequestService.updateW2MReturnRequestById(req.params.id, req.body);
+  const result = await rtoMReturnRequestService.updateMtoRReturnRequestById(req.params.id, req.body);
   res.send(result);
 });
 
 const deleteReturnRequestById = catchAsync(async (req, res) => {
-  await wToMReturnRequestService.deleteW2MReturnRequestById(req.params.id);
+  await rtoMReturnRequestService.deleteMtoRReturnRequestById(req.params.id);
   res.status(httpStatus.NO_CONTENT).send();
 });
 
--- src/services/type2.services/manufacture.dashboard.count.service.js
+++ src/services/type2.services/manufacture.dashboard.count.service.js
@@ -366,40 +366,6 @@ const getReferredUsersDashboardCounts = async (refByEmail) => {
   };
 };
 
-// const getRequestDashboardCounts = async ({ email, role }) => {
-//   const matchCondition = role === 'manufacture' ? { email } : { email: email };
-
-//   const [result] = await Request.aggregate([
-//     { $match: matchCondition },
-//     {
-//       $group: {
-//         _id: null,
-//         total: { $sum: 1 },
-
-//         pending: {
-//           $sum: { $cond: [{ $eq: ['$status', 'pending'] }, 1, 0] },
-//         },
-
-//         accepted: {
-//           $sum: { $cond: [{ $eq: ['$status', 'accepted'] }, 1, 0] },
-//         },
-
-//         rejected: {
-//           $sum: { $cond: [{ $eq: ['$status', 'rejected'] }, 1, 0] },
-//         },
-//       },
-//     },
-//   ]);
-
-//   return {
-//     requests: {
-//       total: result?.[0]?.total || 0,
-//       pending: result?.[0]?.pending || 0,
-//       accepted: result?.[0]?.accepted || 0,
-//       rejected: result?.[0]?.rejected || 0,
-//     },
-//   };
-// };
 
 const getRequestDashboardCounts = async ({ email, role }) => {
   const normalizedEmail = email?.toLowerCase().trim();
